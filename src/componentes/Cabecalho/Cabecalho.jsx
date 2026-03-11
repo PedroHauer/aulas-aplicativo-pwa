954e10ae89a42fbae1583748b6baeb7e9a4ada82
PEDRO HENRIQUE HAUER CARVALHO GALVÃO <pedro_h_galvao@estudante.sesisenai.org.br>
2026-03-10 21:59:16 -0300

    Att

--- src/componentes/Cabecalho/Cabecalho.jsx
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -5,7 +5,6 @@ function Cabecalho() {
   return (
     <header className="cabecalho__root">
       <img src="/logo.png" height="32" alt="Logo" />
-      <div className="cabecalho__avatar">PH</div>
       <Avatar nome="Pedro Henrique Hauer" />
     </header>
   );
--- src/componentes/Principal/Principal.jsx
+++ src/componentes/Principal/Principal.jsx
@@ -1,7 +1,15 @@
 import "./Principal.css";
+import BotaoCustomizado from "../BotaoCustomizado/BotaoCustomizado";
+
 
 function Principal() {
-  return <main className="principal__root">Principal</main>;
+  return (
+  <main className="principal__root">
+    Principal
+ <button class="botao-customizado__root">Salvar</button>
+ <button class="botao-customizado__root">Cancelar</button>
+  </main>
+  );
 }
 
 export default Principal; 
