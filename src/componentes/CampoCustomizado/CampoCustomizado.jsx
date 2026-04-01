28042f61d2a43a6721a88f43fe095bc5e275ce80
PEDRO HENRIQUE HAUER CARVALHO GALVÃO <pedro_h_galvao@estudante.sesisenai.org.br>
2026-03-31 21:43:48 -0300

    Att

--- src/componentes/CampoCustomizado/CampoCustomizado.jsx
+++ src/componentes/CampoCustomizado/CampoCustomizado.jsx
@@ -1,13 +1,12 @@
-import "./CampoCustomizado.css"
+import "./CampoCustomizado.css";
 
-function CampoCustomizado({label, ...props}){
-    
-    return (
-        <div className="campo-customizado__root"> 
-        <label>Campo Customizado</label>
-            <input  className="campo-customizado__input" {...props} />
-        </div>
-    );
+function CampoCustomizado({ label, ...props }) {
+  return (
+    <div className="campo-customizado__root">
+      <label>{label}</label>
+      <input className="campo-customizado__input" {...props} />
+    </div>
+  );
 }
 
 export default CampoCustomizado;
--- src/paginas/ListaTarefas/ListaTarefas.jsx
+++ src/paginas/ListaTarefas/ListaTarefas.jsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
-import Principal from "../../componentes/Principal/principal";
+import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado";
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado";
+import Principal from "../../componentes/Principal/Principal";
 
-function ListaTarefas({}){
-   const [novaTarefa, setNovaTarefa] = useState("");
+function ListaTarefas({}) {
+  const [novaTarefa, setNovaTarefa] = useState("");
+const [tarefas, setTarefas] = useState([]);
 
-    return ( 
+const adicionarTarefa = () => {
+if (novaTarefa.trim() === "") {
+  alert("Digite uma tarefa para adicionar!");
+  return;
+}
+
+  console.log(novaTarefa);
+
+  setTarefas([novaTarefa, ...tarefas]);
+};
+
+  return (
     <Principal>
-        <h2>Lista de Tarefas</h2>
+      <h2>Lista de Tarefas</h2>
 
-   <CampoCustomizado
-    type="text"
-  placeholder="Nova Tarefa"
-   value={novaTarefa}
-   onChange={(event) => setNovaTarefa(event.target.value)}
-   />
+      <CampoCustomizado
+        label="Nova Tarefa"
+        type="text"
+        placeholder="Precione Enter para adicionar"
+        value={novaTarefa}
+        onChange={(event) => setNovaTarefa(event.target.value)}
+      />
+
+<BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>
+  +
+  </BotaoCustomizado>
+    
+    {JSON.stringify(tarefas, null, 2)}
     </Principal>
-    );
+  );
 }
 
 export default ListaTarefas;
